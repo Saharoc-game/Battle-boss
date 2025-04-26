b6e760acdc0791f388ecc0d1e2b29395776d5b75
Saharoc-game <dsilakov01@gmail.com>
2025-04-26 16:37:29 +0300

    start

---
+++ main.py
@@ -0,0 +1,36 @@
+import arcade
+
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 1080
+
+
+class BatleBoss(arcade.Window):
+    """ Главный класс приложения. """
+
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def setup(self):
+        # Настроить игру здесь
+        pass
+
+    def on_draw(self):
+        """ Отрендерить этот экран. """
+        arcade.start_render()
+        # Здесь код рисунка
+
+    def update(self, delta_time):
+        """ Здесь вся игровая логика"""
+        pass
+
+
+def main():
+    game = BatleBoss(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main() 

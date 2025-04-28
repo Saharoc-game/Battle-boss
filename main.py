6201a418c5e092cc7d1c404a9a97434f495f3ecb
Saharoc-game <dsilakov01@gmail.com>
2025-04-28 20:42:07 +0300

    ы

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class BattleBoss(arcade.Window):
 
     def __init__(self, width, height, title="BattleBoss"):
         super().__init__(width, height, title)
-        self
-
+        self.player = player.choose_playerclass()
+        self.boss = boss.random_boss()
         arcade.set_background_color(arcade.color.BLACK)
 
     def setup(self):
@@ -25,7 +25,15 @@ class BattleBoss(arcade.Window):
     def on_draw(self):
         """ Отрендерить этот экран. """
         arcade.start_render()
-        # Здесь код рисунка
+         # Отображение информации об игроке
+        arcade.draw_text(f"Здоровье игрока: {self.player.hp}", 10, SCREEN_HEIGHT - 20, arcade.color.WHITE, 14)
+        arcade.draw_text(f"Магия игрока: {self.player.magic}", 10, SCREEN_HEIGHT - 40, arcade.color.WHITE, 14)
+        arcade.draw_text(f"Деньги игрока: {self.player.money}", 10, SCREEN_HEIGHT - 60, arcade.color.WHITE, 14)
+        # Отображение информации о боссе
+        arcade.draw_text(f"Здоровье босса: {self.boss.hp}", SCREEN_WIDTH - 200, SCREEN_HEIGHT - 20, arcade.color.WHITE, 14)
+        arcade.draw_text(f"Магия босса: {self.boss.magic}", SCREEN_WIDTH - 100, SCREEN_HEIGHT - 20, arcade.color.WHITE, 14)
+
+
 
     def update(self, delta_time):
         """ Здесь вся игровая логика"""

6155b03347d33091722cd51ece3fa43a8a1369aa
Saharoc-game <dsilakov01@gmail.com>
2025-05-21 19:46:42 +0300

    start

---
+++ core/effect/fire.py
@@ -0,0 +1,17 @@
+from core.effect.poison import PosionEffect
+
+class FireEffect(PosionEffect) :
+     
+    """Эффект Огонь: Каждый ход отнимает damage. Длится duration ходов. Каждый ход снижает урон на 30% """
+
+    def __init__(self, duration: int, damage: int):
+        """ Создаём новый эффект. Принимаем duration и damage"""
+        super().__init__(duration)
+        print(f"Вас подожгли! Вы будете гореть {duration} ходов. А так же сила {damage} ")
+        self.damage = damage
+
+    def apply(self, target):
+        """Обновляем эффект. Отнимаем у target урон эффекта. Также снижаем damage на 30%"""
+        target.health -= self.damage
+        self.damage = int(self.damage - (self.damage * 0.3))
+

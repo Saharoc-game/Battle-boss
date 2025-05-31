5e1e4a55ced3786d0e6c8191b87e605a4eb7a626
Saharoc-game <dsilakov01@gmail.com>
2025-05-31 15:31:17 +0300

    fix

--- core/effect/__init__.py
+++ core/effect/__init__.py
@@ -2,3 +2,4 @@ from .poison import PosionEffect
 from .fire import FireEffect
 from .bleeding import BleedingEffect
 from .stun import StunEffect
+from .advantage import AdvantageEffect
--- core/effect/advantage.py
+++ core/effect/advantage.py
@@ -5,7 +5,8 @@ class AdvantageEffect(Effect) :
 
     def __init__(self):
         """Создаём эффект. Длится duration ходов """
-        self.duration = 1
+        self.duration = 2
+        print(f"У вас перевес! Ваш урон снижен на 30%")
 
     def apply(self, target):
         target.advantage = 30

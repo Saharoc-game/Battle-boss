1bc21e8ebf6330fbe7916024effe1e59cbca77f5
Saharoc-game <dsilakov01@gmail.com>
2025-06-01 23:05:44 +0300

    fix

--- BattleBoss.py
+++ BattleBoss.py
@@ -1,10 +1,8 @@
 import random
 from core import boss 
 from core import player
-
-from core.effect import PosionEffect, BleedingEffect, FireEffect, StunEffect
+from core.effect import PoisonEffect, BleedingEffect, FireEffect, StunEffect
 from utils.input_until import get_valid_int_input
-
 
 print("Привет, игрок. Ты играешь в игру Batle Boss.")
 

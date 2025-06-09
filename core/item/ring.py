9508af2344e731d4e23e6108c225f61d6b89ec33
Saharoc-game <dsilakov01@gmail.com>
2025-06-09 15:55:24 +0300

    and add shop

--- core/item/ring.py
+++ core/item/ring.py
@@ -2,17 +2,21 @@ import random
 
 
 class Ring():
-    def __init__(self):
-        self.heal = random.randint(1,3)
-        self.cost = self.heal+ 1
-        self.name = "Кольцо регенерации"
-        self.description = "Красивое железное кольцо с зелёным камнем"
-        self.weight = 0.5
+
+    def __init__(self, heal=None, cost=None, name="Кольцо регенерации", 
+                 description="Красивое железное кольцо с зелёным камнем", 
+                 weight=0.5):
+        self.heal = random.randint(1,3) if heal is None else heal
+        self.cost = self.heal + 1 if cost is None else cost
+        self.name = name
+        self.description = description
+        self.weight = weight
         self.type = "ring"
+
     def create(self):
         x = {"type": self.type,
             "cost": self.cost,
-            "heal": self.effect,
+            "heal": self.heal,
             "name": self.name,
             "description": self.description,
             "weight": self.weight}
